ae43277b49443c7a38772afa2d9b5e03ae425dfa
ujwal284 <ujwalkhanal284@gmail.com>
2024-09-30 16:36:18 +0545

    created to-do app

---
+++ index.js
@@ -0,0 +1,60 @@
+// form validation
+
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+
+    formValidation();
+});
+
+let formValidation = () => {
+    if (input.value === "") {
+
+    } else {
+        acceptData();
+    }
+};
+
+
+
+// accept data from input fields
+let data = {};
+
+let acceptData = () => {
+    data["text"] = input.value;
+
+
+    createPost();
+};
+
+
+
+// create a post
+let createPost = () => {
+    posts.innerHTML += `
+        <div>
+        <p>${data.text}</p>
+        <span class = "options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+        </span>
+        </div>
+    `;
+
+    input.value = "";
+};
+
+
+
+// delete a post
+
+let deletePost = (e) => {
+
+    e.parentElement.parentElement.remove();
+};
