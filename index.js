a677584a7a2b76dc49345a90339c5367a6b76a71
ujwal284 <ujwalkhanal284@gmail.com>
2024-09-30 18:07:48 +0545

    added js

---
+++ index.js
@@ -0,0 +1,49 @@
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
+
+// Form Validations
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formValidation();
+});
+
+let formValidation = () => {
+    if (textInput.value === "") {
+        console.log("failure");
+        msg.innerHTML = "Task cannot be blank";
+    } else {
+        console.log("success");
+        msg.innerHTML = "";
+
+        acceptData();
+        add.setAttribute("data-bs-dismiss", "modal");
+        add.click();
+
+        (() => {
+            add.setAttribute("data-bs-dismiss", "");
+        })();
+    }
+};
+
+
+// collect data and use local storage
+
+let data = [];
+
+let acceptData = () => {
+    data.push({
+        text: textInput.value,
+        data: dataInput.value,
+        description: textarea.value,
+    });
+
+    localStorage.setItem("data", JSON.stringify(data));
+
+    console.log(data);
+}
